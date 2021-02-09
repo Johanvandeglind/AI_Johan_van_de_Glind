283fe0c40844f9fd702964af7a7ffe1944b6a4a5
Johanvandeglind <31281194+Johanvandeglind@users.noreply.github.com>
2021-02-09 23:16:14 +0100

    Startegie eigen (verry deluce) werkt bijna ok

--- Modules/Generator.py
+++ Modules/Generator.py
@@ -9,13 +9,14 @@ def Generate(feedback:dict,round:str,):
                 code[i] = randint(0,5)
         print(code)
         return code
-    #if round == 'round_1':
-        #for i in range(0,feedback['round_0']['black_pins']):
-    #return code
+
 
 
 
 def game_player_gueses():
+    """
+
+    """
     feedback = dict(round_0=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
                     round_1=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
                     round_2=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
@@ -45,27 +46,34 @@ def game_player_gueses():
 
 
 def raad_code_user(feedback:dict,round:int):
-    '''
+    """
 
-    '''
+    @param feedback:
+    @param round:
+    @return:
+    """
     if round == 0:
         inp_code= user_color_code()
     elif round == 9:
         print('Dit is uw laatste kans')
         print("-----------------------------------------------------------------------------------------------")
-        for i in range(0, round):
-            uitschfijven_feedback(round,i,feedback)
+        uitschfijven_feedback(round,feedback)
         inp_code= user_color_code()
     else:
         print("-----------------------------------------------------------------------------------------------")
-        for i in range(0,round):
-            uitschfijven_feedback(round, i, feedback)
+        uitschfijven_feedback(round, feedback)
         inp_code =user_color_code()
     return inp_code
 
 
 
-def uitschfijven_feedback(round:int,i:int,feedback:dict):
+def uitschfijven_feedback(round:int,feedback:dict):
+    """
+
+    @param round:
+    @param feedback:
+    @return:
+    """
     kleuren_combi = ['Zwart', 'Wit', 'Rood', 'Geel', 'Blauw', 'Groen']
     for i in range(0, round):
         print(f'Feedback {i + 1}e ronde:\n'
@@ -75,15 +83,12 @@ def uitschfijven_feedback(round:int,i:int,feedback:dict):
         print("-----------------------------------------------------------------------------------------------")
 
 def user_color_code():
-    '''
+    """
     Fucntie voor het invoeren van de code van user (raden of bij het starten)
-    '''
-    inp_code = []
-    kleuren = ['1:Zwart', '2:Wit', '3:Rood', '4:Geel', '5:Blauw', '6:Groen']
-    for i in range(0, 4):
-        inp_code.append(int(input(
-            f'Wat is de {i + 1}e kleur, maak een keuze uit deze 6 kleuren (geef uw nummer op):\n1:Zwart\n2:Wit\n3:Rood\n4:Geel\n5:Blauw\n6:Groen\n')) - 1)
-        print(f'Uw keuze was {kleuren[inp_code[i]]}')
+    @return: de ingevoerde code van de user
+    """
+    inp_code=(list(input(f'Wat zijn de kleuren, maak een keuze uit deze 6 kleuren (geef zo 1234 uw nummers op):\n0:Zwart\n1:Wit\n2:Rood\n3:Geel\n4:Blauw\n5:Groen\n')))
+    inp_code =[int(item) for item in inp_code]
     return inp_code
 
 
--- Modules/Strategieën.py
+++ Modules/Strategieën.py
@@ -1,6 +1,7 @@
 import itertools
 import random
 from Feedback import givefeedback
+
 feedback = dict(round_0=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
                 round_1=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
                 round_2=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
@@ -10,43 +11,166 @@ feedback = dict(round_0=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
                 round_6=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
                 round_7=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
                 round_8=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
-                round_9=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0])
-                )
+                round_9=dict(black_pins=0, white_pins=0, old_code=[0, 0, 0, 0]),
+                black=0,
+                white=0,
+                red=0,
+                yellow=0,
+                blue=0,
+                green=0)
 
-def simple_strat(round:int,feedback:dict):
-    print(feedback)
-    code=[]
+
+def simple_strat(round: int, feedback: dict):
+    """
+    @param round:
+    @param feedback:
+    @return:
+    """
+
+    print(feedback["black"],feedback["white"],feedback["red"],feedback["yellow"],feedback["blue"],feedback["green"])
+    code = []
+    combi_lst = []
     if round == 0:
-        code = [0, 1, 2, 3]
+        code = [0, 0, 0, 0]
         return code
-    if round == 1:
-        if feedback["round_0"]["white_pins"] == 4:
-            possible_combinations(feedback["round_0"]["old_code"])
-        elif feedback["round_0"]["white_pins"] == 0:
-            code = possible_combinations([4,5])[random.randint(0,len(possible_combinations))]
+    elif len(combi_lst) > 0:
+        lst = possible_combinations(combi_lst,feedback)
+        lst = remove_doubles_code_lst(lst, combi_lst,feedback)
+        print(lst)
+    elif round == 1:
+        print(feedback["round_0"]["black_pins"])
+        feedback["black"] = feedback["round_0"]["black_pins"]
+        return [1, 1, 1, 1]
+    elif round == 2:
+        print(feedback["round_1"]["black_pins"])
+        feedback["white"] = feedback["round_1"]["black_pins"]
+
+        if feedback["black"] + feedback["white"] == 4:
+            combi_lst = check_right_colors(feedback)
+            return [2, 2, 2, 2]
+        else:
+            return [2, 2, 2, 2]
+    elif round == 3 and len(combi_lst) == 0:
+        print(feedback["round_2"]["black_pins"])
+        feedback["red"] = feedback["round_2"]["black_pins"]
+
+        if feedback["black"] + feedback["white"]+ feedback["red"] == 4:
+            combi_lst = check_right_colors(feedback)
+            return [3, 3, 3, 3]
+        else:
+            return [3, 3, 3, 3]
+    elif round == 4and len(combi_lst) == 0:
+        print(feedback["round_3"]["black_pins"])
+        feedback["yellow"] = feedback["round_3"]["black_pins"]
+
+        if feedback["black"] + feedback["white"] + feedback["red"]+feedback["yellow"]==4:
+            combi_lst = check_right_colors(feedback)
+            print(combi_lst)
+            return [4, 4, 4, 4]
+        else:
+            return[4,4,4,4]
+    elif round == 5and len(combi_lst) == 0:
+        feedback["blue"] = feedback["round_4"]["black_pins"]
+
+        if feedback["black"] + feedback["white"] + feedback["red"] + feedback["yellow"] == 4:
+            combi_lst = check_right_colors(feedback)
+            return [5, 5, 5, 5]
+        else:
+            return [5, 5, 5, 5]
+    elif round == 6 and len(combi_lst) == 0:
+        feedback["green"] = feedback["round_5"]["black_pins"]
+        combi_lst = check_right_colors(feedback)
+        return [6, 6, 6, 6]
+    elif round == 7 and len(combi_lst) == 0:
+        feedback["green"] = feedback["round_5"]["black_pins"]
+        combi_lst = check_right_colors(feedback)
+        lst = possible_combinations(combi_lst)
+        lst = remove_doubles_code_lst(lst,combi_lst,feedback)
+        print(lst)
+
+
     return code
 
 
+def check_right_colors(feedback:dict):
+    combi_lst =[]
+    if feedback["black"] > 0:
+        combi_lst.append(0)
+    if feedback["white"] > 0:
+        combi_lst.append(1)
+    if feedback["red"] > 0:
+        combi_lst.append(2)
+    if feedback["yellow"] > 0:
+        combi_lst.append(3)
+    if feedback["blue"] > 0:
+        combi_lst.append(4)
+    if feedback["green"] > 0:
+        combi_lst.append(5)
+    return combi_lst
+
+def remove_doubles_code_lst(lst:list,combi_lst:list,feedback:dict):
+    if len(combi_lst) == 4:
+        remove_index =[]
+        for item in lst:
+            if combi_lst[0] in item and combi_lst[1] in item and combi_lst[2] in item and combi_lst[3] in item:
+                remove_index.append((item))
+        return remove_index
+    if len(combi_lst) == 3:
+        double = -1
+        remove_index =[]
+        kleurcode = ['black','white','red','yellow','blue','green']
+        for i in range(0,6):
+            if feedback[kleurcode[i]] == 2:
+                double = i
+        for item in lst:
+            if combi_lst[0] in item and combi_lst[1] in item and combi_lst[2] in item:
+                if item.count(double) == 2:
+                    remove_index.append((item))
+        return remove_index
+    if len(combi_lst) == 2:
+        remove_index =[]
+        kleurcode = ['black','white','red','yellow','blue','green']
+        for i in range(0,6):
+            if feedback[kleurcode[i]] == 2:
+                for item in lst:
+                    if combi_lst[0] in item and combi_lst[1] in item:
+                        if item.count(combi_lst[0]) == 2 and item.count(combi_lst[1]) == 2:
+                            remove_index.append((item))
+            elif feedback[kleurcode[i]] == 3:
+                for item in lst:
+                    if combi_lst[0] in item and combi_lst[1] in item:
+                        if item.count(combi_lst[i]) == 3:
+                            remove_index.append((item))
+
+        return remove_index
 
 def possible_combinations(code):
+    """
+
+    @param code:
+    @return:
+    """
     valid_chars = code
     all_combinations = (list(itertools.product(valid_chars, repeat=4)))  # <= list with tuples of 4 ('B', 'B', 'B', 'B')
     all_combinations = [list(i) for i in all_combinations]
     return all_combinations
 
 
-all_combs = possible_combinations([0,1,2,3,4,5])
-code = all_combs[random.randint(0,len(all_combs))]
-
-
-for i in range(0, 3):
+all_combs = possible_combinations([0, 1, 2, 3, 4, 5])
+code = all_combs[random.randint(0, len(all_combs))]
+print(code)
+for i in range(0, 11):
     if i == 0:
-        print(f'======================================== Round {i + 1} ======================================================')
-        givefeedback(code, simple_strat( i,feedback), feedback, f'round_{str(i)}')
+        print(
+            f'======================================== Round {i + 1} ======================================================')
+        givefeedback(code, simple_strat(i, feedback), feedback, f'round_{str(i)}')
+    elif i == 10:
+        print("U heeft de code niet geraden, volgende keer beter :D")
+        break
     else:
         if feedback[f"round_{str(i - 1)}"]["black_pins"] != 4:
             print(f'======================================== Round {i + 1} ======================================================')
-            givefeedback(code, simple_strat( i,feedback), feedback, f'round_{str(i)}')
+            givefeedback(code, simple_strat(i, feedback), feedback, f'round_{str(i)}')
         else:
             print("Gefeliciteerd u heeft de code goed geraden!!")
             break
